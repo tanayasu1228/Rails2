0e8a033333c5e96a1d7ef7964225590d7fe3e143
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-03-30 09:51:31 +0900

    used_resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  # 一覧画面へのルーティング
-  get "/users", to: "users#index"
-
-  # 新規投稿画面へのルーティング
-  get "/users/new", to: "users#new"
-
-  # 新規登録を行う為のルーティング
-  post "/users", to: "users#create"
-
-  # 編集画面へのルーティング
-  get "/users/:id/edit", to: "users#edit"
-
-  # 更新を行う為のルーティング
-  patch "/users/:id", to: "users#update"
-
-  # 削除を行うためのルーティング
-  delete "/users/:id", to: "users#destroy"
+resources :users
 end
